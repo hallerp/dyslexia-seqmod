40314cfe1c57ca39258b4da62a78034192df1e10
Patrick Haller <patrick.hlr@gmail.com>
2022-11-04 10:50:16 +0100

    Update notify.py

--- src/notify.py
+++ src/notify.py
@@ -3,10 +3,10 @@ import smtplib
 
 class Notification:
     def __init__(self):
-        self.email_user = 'patrick.hlr@gmail.com'
-        self.server = smtplib.SMTP ('smtp.gmail.com', 587)
+        self.email_user = ''
+        self.server = smtplib.SMTP ('', )
         self.server.starttls()
-        self.server.login(self.email_user, 'wmlbdmujhphrycjv')
+        self.server.login(self.email_user, '')
 
     def define_message(self, message, failed=False):
         if failed:
